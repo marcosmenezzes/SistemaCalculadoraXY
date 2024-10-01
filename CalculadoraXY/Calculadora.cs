2a596241b4c27587438e22290f54a3e916ecac3d
Senai <Senai@CEPEAS21103954>
2024-09-30 21:39:38 -0300

    Add project files.

---
+++ CalculadoraXY/Calculadora.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraXY
+{
+    internal class Calculadora
+    {
+
+        public static double SomarNumeros(double numero1, double numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public static double SubtrairNumeros(double numero1, double numero2)
+
+        {
+            return numero1 - numero2;
+        }
+
+        public static double MultiplicarNumeros(double numero1, double numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public static double DividirNumeros(double numero1, double numero2)
+        {
+            if (numero2 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (numero1 / numero2);
+            }
+        }
+
+        public static double PotenciaNumeros(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        public static string Baskara1(double a, double b, double c)
+        {
+            double delta = (b * b) - 4 * a * c;
+            double x1 = 0;
+            double x2 = 0;
+
+            if (delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                return "X1 = " + x1.ToString() + "\nX2 = " + x2.ToString();
+            }
+            else
+            {
+                if (delta == 0)
+                {
+                    x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+                    return "X1 e X2 = " + x1.ToString();
+                }
+                else
+                {
+                    return "Não existe raiz real.";
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ CalculadoraXY/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace CalculadoraXY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            double num1;
+            double num2; 
+            double num3;
+
+            Console.Write("Insira o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Insira o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.Write("Insira o terceiro número: ");
+            num3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("O resultado da adição é: " + Calculadora.SomarNumeros(num1,num2));
+            Console.WriteLine();
+            Console.Write("O resultado da subtração de (x,y): " + Calculadora.SubtrairNumeros(num1,num2));
+            Console.WriteLine();
+            Console.Write("O resultado da divisão de (x,y): " + Calculadora.DividirNumeros(num1, num2));
+            Console.WriteLine();
+            Console.Write("O resultado da multiplicação de (x,y): " + Calculadora.MultiplicarNumeros(num1, num2));
+            Console.WriteLine();
+            Console.Write("O resultado da potência de (x,y): " + Calculadora.PotenciaNumeros(num1, num2));
+            Console.WriteLine();
+            Console.Write("O resultado da equação 2º grau: "+ "\n" + Calculadora.Baskara1(num1, num2, num3));
+      
+            Console.ReadKey();
+         
+
+        }
+    }
+}
